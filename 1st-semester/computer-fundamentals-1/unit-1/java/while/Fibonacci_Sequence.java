b74ee00032a6803900c3c43aa9ef808d96a17c6b
Santiago <sbeltranastorga@gmail.com>
2025-11-06 09:16:11 -0700

    Add 'while' exercises in Java

---
+++ 1st-semester/computer-fundamentals-1/unit-1/java/while/Fibonacci_Sequence.java
@@ -0,0 +1,36 @@
+/*
+2. Escribir un programa para encontrar la secuencia de Fibonacci. La secuencia es al 
+siguiente: 
+ 
+F= 0,1,1,2,3,5,8,13,21,34,55, ......................................., n 
+ 
+El primer elemento es 0, el segundo es 1 y cada elemento restante es la suma de los 
+dos anteriores. El programa debe solicitar el número límite de la serie.
+*/
+import java.util.Scanner;
+public class Fibonacci_Sequence {
+    public static void main (String[] args) {
+        int limit;
+        String option = "Y";
+        Scanner input = new Scanner(System.in);
+        while (option.equalsIgnoreCase("Y")) {
+            System.out.println("\nInput a number to establish the limit of the sequence: ");
+            limit = input.nextInt();
+            if (limit > 0) {
+                int element0 = 0, element1 = 1, sum = 0;
+                System.out.printf("%d, %d", element0, element1);
+                while (sum < limit) {
+                    sum = element0 + element1;
+                    System.out.printf(", %d", sum);
+                    element0 = element1;
+                    element1 = sum;
+                }
+                option = System.console().readLine("\n\nAgain? Y/N: ");
+            } else {
+                System.out.println("ERROR. Only positive integers are allowed.");
+            }
+        }
+        System.out.println("Thank you for using the program :)");
+        input.close();
+    }
+}
---
+++ 1st-semester/computer-fundamentals-1/unit-1/java/while/Numbers_Summation.java
@@ -0,0 +1,38 @@
+/*
+3. Sumatoria encontrar la sumatoria de una serie de números, el limite se le solicita al 
+usuario. 
+ 
+Escriba el número para sumatoria: 8 
+Resultado: 36 
+ 
+Σ= 1 + 2 + 3 +4 +5 + 6 + 7 +8 
+*/
+import java.util.Scanner;
+public class Numbers_Summation {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        int limit;
+        String option = "Y";
+        while (option.equalsIgnoreCase("Y")) {
+            int number = 1, summation = 0;
+            System.out.println("\nEstablish a limit for the sequence of numbers: "); limit = input.nextInt();
+            if (limit > 0) {
+                while (number <= limit) {
+                    if (number < limit) {
+                        System.out.printf("%d + ", number);
+                    } else {
+                      System.out.printf("%d", number);
+                    }
+                    summation += number;
+                    number++;
+                }
+                System.out.println("\nSummation: " + summation);
+                option = System.console().readLine("\nAgain? Y/N: ");
+            } else {
+                System.out.println("ERROR. Only positive integers are allowed.");
+            }
+        }
+        System.out.println("Thank you for using the program :)");
+        input.close();
+    }
+}
